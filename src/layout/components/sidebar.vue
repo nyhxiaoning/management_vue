68a3419ab2cb2d284376d0ba5fc2785aeff54e0e
henry <1540097293@qq.com>
2025-05-08 22:07:26 +0800

    feat: 配置当前的npm包管理:yarn安全感

--- src/layout/components/sidebar.vue
+++ src/layout/components/sidebar.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="h-[100%] bg-[#545c64]">
     <div class="h-[50px] text-white flex items-center justify-center">
-      <span v-if="!homeStore.isCollapse">权限管理系统</span>
+      <span>权限管理系统</span>
     </div>
     <el-scrollbar class="wrap-scroll">
-      <el-menu :unique-opened="true" :collapse="homeStore.isCollapse" active-text-color="#ffd04b" @select="getPath"
-        background-color="#545c64" class="el-menu-vertical-demo w-[223px] !border-r-0" :default-active="activeMenu"
-        text-color="#fff">
-        <sidebaritem v-for="item in homeStore.menuList" :key="item.id!" :item="item" />
+      <el-menu
+        :unique-opened="true"
+        :collapse="homeStore.isCollapse"
+        active-text-color="#ffd04b"
+        @select="getPath"
+        background-color="#545c64"
+        class="el-menu-vertical-demo w-[223px] !border-r-0"
+        :default-active="activeMenu"
+        text-color="#fff"
+      >
+        <sidebaritem
+          v-for="item in homeStore.menuList"
+          :key="item.id!"
+          :item="item"
+        />
       </el-menu>
     </el-scrollbar>
   </div>
